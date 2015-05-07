65540ca6573b25f6edb05fa4e6a8cb3850c07615
SvenDingens <sven.dingens@gmail.com>
2015-05-07 19:59:54 +0200

    initial commit
    Signed-off-by: SvenDingens <sven.dingens@gmail.com>

---
+++ app/src/main/java/com/example/sven/funfacts/FactBook.java
@@ -0,0 +1,37 @@
+package com.example.sven.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by Sven on 07/05/2015.
+ */
+public class FactBook {
+
+    public String [] mFacts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver.",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England.",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the earth when the Great Pyramid was being built.",
+            "Elephants can detact rain 240 kilometers away"
+    };
+
+    public String getFact() {
+
+        String facts = "";
+        // Random generator
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.nextInt(mFacts.length);
+
+        facts = mFacts[randomNumber];
+
+        return facts;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/sven/funfacts/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.sven.funfacts;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+
+public class MainActivity extends Activity {
+
+    public static final String TAG  = MainActivity.class.getSimpleName();
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Declare our View variables
+        final TextView factLabel = (TextView) findViewById(R.id.textView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout =(RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String facts = mFactBook.getFact();
+                factLabel.setText(facts);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        //Toast.makeText(this, "Welcome to my App powered by Sven", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
